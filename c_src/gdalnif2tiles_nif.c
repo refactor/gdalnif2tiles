1e146410d3b9209868b235512e7b93e34ee70cda
wulei <wulei@digitalearth.cn>
2019-01-06 16:29:37 +0800

    get dataset info & band info

--- c_src/gdalnif2tiles_nif.c
+++ c_src/gdalnif2tiles_nif.c
@@ -4,15 +4,27 @@
 #include <gdal.h>
 #include <cpl_conv.h>
 
+#include "utils.h"
+
 static ErlNifResourceType* imageBinResType;
 
 typedef struct MyGDALDataset {
     GDALDatasetH handle;
+    int rasterCount;
+    int srid;
+    int rasterWidth, rasterHeight;
+    double originX;
+    double originY;
+    double pixelWidth;
+    double pixelHeight;
+    double minBoundX, maxBoundX;
+    double minBoundY, maxBoundY;
+    double minBoundZ, maxBoundZ;
 } MyGDALDataset;
 
 static ERL_NIF_TERM ATOM_OK;
 
-void image_dtor(ErlNifEnv* env, void* obj) {
+void dataset_dtor(ErlNifEnv* env, void* obj) {
     MyGDALDataset *pGDALDataset = (MyGDALDataset*)obj;
     LOGA("pGDALDataset -> %p, handle -> %p", pGDALDataset, pGDALDataset->handle);
     if (pGDALDataset && pGDALDataset->handle != NULL) {
@@ -23,14 +35,27 @@ void image_dtor(ErlNifEnv* env, void* obj) {
 static ERL_NIF_TERM open_file(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]) {
     char filename[128] = {};
     if (!enif_get_string(env, argv[0], filename, sizeof(filename), ERL_NIF_LATIN1)) {
-        return enif_make_badarg(env);
+        return enif_raise_exception(env,
+            enif_make_string(env, "No input file was specified", ERL_NIF_LATIN1));
     }
     GDALDatasetH hDataset = GDALOpen(filename, GA_ReadOnly);
     if (hDataset == NULL) {
-        return enif_raise_exception(env, enif_make_atom(env, "open_failed"));
+        return enif_raise_exception(env,
+            enif_make_string(env, "It is not possible to open the input file", ERL_NIF_LATIN1));
     }
     MyGDALDataset *pGDALDataset = enif_alloc_resource(imageBinResType, sizeof(*pGDALDataset));
     pGDALDataset->handle = hDataset;
+    pGDALDataset->rasterWidth = GDALGetRasterXSize(hDataset);
+    pGDALDataset->rasterHeight = GDALGetRasterYSize(hDataset);
+    pGDALDataset->rasterCount = GDALGetRasterCount(hDataset);
+    double adfGeoTransform[6];
+    if (GDALGetGeoTransform(hDataset, adfGeoTransform) == CE_None) {
+        pGDALDataset->originX = adfGeoTransform[0];
+        pGDALDataset->originY = adfGeoTransform[3];
+        pGDALDataset->pixelWidth = adfGeoTransform[1];
+        pGDALDataset->pixelHeight = adfGeoTransform[5];
+        LOGA("coeff[2]: %f, coeff[4]: %f", adfGeoTransform[2],adfGeoTransform[4]);
+    }
     LOGA("open success: res -> %p, handle -> %p", pGDALDataset, hDataset);
     ERL_NIF_TERM res = enif_make_resource(env, pGDALDataset);
     enif_release_resource(pGDALDataset);
@@ -55,16 +80,16 @@ static ERL_NIF_TERM info(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]) {
             enif_make_string(env, GDALGetDriverLongName(hDriver), ERL_NIF_LATIN1),
             &res);
     enif_make_map_put(env, res, 
-            enif_make_atom(env, "xsize"),
-            enif_make_double(env, GDALGetRasterXSize(hDataset)),
+            enif_make_atom(env, "raster_width"),
+            enif_make_int(env, pGDALDataset->rasterWidth),
             &res);
     enif_make_map_put(env, res, 
-            enif_make_atom(env, "ysize"),
-            enif_make_double(env, GDALGetRasterYSize(hDataset)),
+            enif_make_atom(env, "raster_height"),
+            enif_make_int(env, pGDALDataset->rasterHeight),
             &res);
     enif_make_map_put(env, res, 
             enif_make_atom(env, "bands"),
-            enif_make_int(env, GDALGetRasterCount(hDataset)),
+            enif_make_int(env, pGDALDataset->rasterCount),
             &res);
     const char* proj = GDALGetProjectionRef(hDataset);
     if (proj != NULL) {
@@ -79,14 +104,14 @@ static ERL_NIF_TERM info(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]) {
         enif_make_map_put(env, res,
                 enif_make_atom(env, "origin"),
                 enif_make_tuple2(env,
-                    enif_make_double(env, adfGeoTransform[0]),
-                    enif_make_double(env, adfGeoTransform[3])),
+                    enif_make_double(env, pGDALDataset->originX),
+                    enif_make_double(env, pGDALDataset->originY)),
                 &res);
         enif_make_map_put(env, res,
                 enif_make_atom(env, "pixel_size"),
                 enif_make_tuple2(env,
-                    enif_make_double(env, adfGeoTransform[1]),
-                    enif_make_double(env, adfGeoTransform[5])),
+                    enif_make_double(env, pGDALDataset->pixelWidth),
+                    enif_make_double(env, pGDALDataset->pixelHeight)),
                 &res);
     }
                     
@@ -116,6 +141,19 @@ static ERL_NIF_TERM band_info(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[
                 enif_make_int(env, nBlockYSize)),
             &res);
 
+    enif_make_map_put(env, res, enif_make_atom(env, "raster_width"),
+                      enif_make_int(env, GDALGetRasterBandXSize(hBand)),
+                      &res);
+    enif_make_map_put(env, res, enif_make_atom(env, "raster_height"),
+                      enif_make_int(env, GDALGetRasterBandYSize(hBand)),
+                      &res);
+
+    enif_make_map_put(env, res, enif_make_atom(env, "pixel_datatype"),
+            enif_make_atom(env, rasterDataType(GDALGetRasterDataType(hBand))),
+            &res);
+    if (NULL == GDALGetRasterColorTable(hBand)) {
+        WARN("need to convert this file to RGB/RGBA by gdal2tiles");
+    }
     int bGotMin, bGotMax;
     double adfMinMax[2] = {
         GDALGetRasterMinimum(hBand, &bGotMin),
@@ -138,7 +176,7 @@ static ERL_NIF_TERM band_info(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[
 static int nifload(ErlNifEnv* env, void **priv_data, ERL_NIF_TERM load_info) {
     GDALAllRegister();
 
-    imageBinResType = enif_open_resource_type(env, NULL, "imagerl", image_dtor,
+    imageBinResType = enif_open_resource_type(env, NULL, "imagerl", dataset_dtor,
             ERL_NIF_RT_CREATE|ERL_NIF_RT_TAKEOVER, NULL);
     ATOM_OK = enif_make_atom(env, "ok");
     return 0;
---
+++ c_src/utils.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <gdal.h>
+
+inline const char* rasterDataType(GDALDataType datatype) {
+    switch (datatype) {
+        case GDT_Float32:
+            return "float32";
+        case GDT_Float64:
+            return "float64";
+        case GDT_Byte:
+            return "byte";
+        case GDT_UInt16:
+            return "uint16";
+        case GDT_Int16:
+            return "int16";
+        case GDT_UInt32:
+            return "uint32";
+        case GDT_Int32:
+            return "int32";
+        case GDT_Unknown:
+            return "unknow";
+    }
+}
