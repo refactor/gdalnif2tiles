be10f129b1e1434f731a2210e42d38c42f762cb8
wulei <wulei@digitalearth.cn>
2019-01-24 08:19:15 +0800

    update no_data values

--- src/gdalnif2tiles.erl
+++ src/gdalnif2tiles.erl
@@ -4,6 +4,7 @@
 -export([info/1]).
 -export([band_info/2]).
 -export([get_pixel/3]).
+-export([update_no_data_values/1]).
 
 -on_load(init/0).
 
@@ -36,3 +37,25 @@ band_info(_Dataset, _BandNo) ->
 -spec get_pixel(reference(), non_neg_integer(), non_neg_integer()) -> list(float()).
 get_pixel(_Dataset, _X, _Y) ->
     erlang:nif_error(notfound).
+
+update_no_data_values(File) ->
+    {XmlDoc,_} = xmerl_scan:file(File),
+    TL = xmerl_lib:simplify_element(XmlDoc),
+    NewTL = add_gdal_warp_options_to_string(TL),
+    % rm header: "<?xml version=\"1.0\"?>"
+    lists:flatten(tl(xmerl:export_simple([NewTL], xmerl_xml))).
+
+add_gdal_warp_options_to_string({Tag, Attributes, Content}) ->
+    NewContent = add_gdal_warp_options(Content),
+    {Tag, Attributes, NewContent}.
+
+add_gdal_warp_options([{'GDALWarpOptions',Attrs,Content} | Rest]) ->
+    [{'GDALWarpOptions',Attrs, add_option(Content)} | Rest];
+add_gdal_warp_options([Head|Rest]) ->
+    [Head | add_gdal_warp_options(Rest)].
+
+add_option([{'Option',_Attrs,_Content}|_Rest] = Content) ->
+    [{'Option', [{name,"UNIFIED_SRC_NODATA"}],["YES"]} | ["\n    "|Content]];
+add_option([Head | Rest]) ->
+    [Head | add_option(Rest)].
+
